b24d110773230f1952bb1e7c8d0d69cd315262cc
grainpowder <siksicking@gmail.com>
2022-11-14 10:22:29 +0900

    define redshift cluster create, delete process

--- aws_setup.py
+++ aws_setup.py
@@ -1,38 +1,19 @@
 import boto3
 import os
+import typer
+import json
+import time
 
-from configparser import ConfigParser
+from configparser import ConfigParser, ExtendedInterpolation
 from typer import Typer
 from logger import make_logger
 
 
 CONFIG_FILE_PATH = f"{os.getcwd()}/dwh.cfg"
-CONFIG = {
-    "DEFAULT": {
-        "admin_profile": "admin.kim",
-        "region": "ap-northeast-2",
-        "dw_port": 5439,
-    },
-    "s3": {
-        "log_data": "s3://udacity-dend/log-data",
-        "log_jsonpath": "s3://udacity-dend/log_json_path.json",
-        "song_data": "s3://udacity-dend/song-data",
-    },
-    "network.vpc": {
-        "cidr": "172.10.0.0/16",
-        "name": "prod",
-        "igw_name": "prod-igw",
-        "sg_name": "redshift",
-    },
-    "network.subnet": {
-        "cidr": "172.10.100.0/24",
-        "name": "dw-pub-c",
-        "rt_name": "rt-pub-c",
-        "az": "%(region)sc",
-    },
-}
+with open(f"{os.getcwd()}/default_config.json", "r") as file:
+    DEFAULT_CONFIG = json.load(file)
 app = Typer()
-config = ConfigParser()
+cp = ConfigParser(interpolation=ExtendedInterpolation())
 logger = make_logger(__name__)
 
 
@@ -40,9 +21,9 @@ logger = make_logger(__name__)
 def create_config():
     if os.path.exists(CONFIG_FILE_PATH):
         os.remove(CONFIG_FILE_PATH)
-    config.read_dict(CONFIG)
+    cp.read_dict(DEFAULT_CONFIG)
     with open(CONFIG_FILE_PATH, "w") as file:
-        config.write(file)
+        cp.write(file)
 
     logger.info(f"Successfully created configuration file in working directory")
 
@@ -51,35 +32,54 @@ def create_config():
 def create_vpc():
     if not os.path.exists(CONFIG_FILE_PATH):
         raise FileNotFoundError(
-            "Configuration file not found. Execute create-config command in advance."
+            "Configuration file not found. Execute create-config command first."
         )
     else:        
-        config.read(CONFIG_FILE_PATH)
-        if config.has_option("network.vpc", "id"):
+        cp.read(CONFIG_FILE_PATH)
+        if cp.has_option("network.vpc", "id"):
             logger.info("Terminate process since VPC is already created")
             exit()
 
     session = boto3.Session(
-        profile_name=config.get("DEFAULT", "admin_profile"), 
-        region_name=config.get("DEFAULT", "region")
+        profile_name=cp.get("DEFAULT", "admin_profile"), 
+        region_name=cp.get("DEFAULT", "region")
     )
     ec2_client = session.client("ec2")
 
-    logger.info("Create VPC and attach internet gateway")
+    # VPC
+    logger.info("Create VPC")
     vpc_info = ec2_client.create_vpc(
-        CidrBlock=config.get("network.vpc", "cidr"),
+        CidrBlock=cp.get("network.vpc", "cidr"),
         TagSpecifications=[
             {
                 "ResourceType": "vpc", 
-                "Tags":[{"Key": "Name", "Value": config.get("network.vpc", "name")}]
+                "Tags":[{"Key": "Name", "Value": cp.get("network.vpc", "name")}]
             }
         ]
     )["Vpc"]
+
+    # Security group
+    logger.info("Create security group with ingress policy")
+    sg_info = ec2_client.create_security_group(
+        GroupName=cp.get("network.vpc", "sg_name"),
+        Description="traffic rules over Redshift cluster",
+        VpcId=vpc_info["VpcId"]
+    )
+    ec2_client.authorize_security_group_ingress(
+        CidrIp="0.0.0.0/0",  # better to allow connection by predefined VPN, but skip for simplicity
+        IpProtocol="tcp",
+        GroupId=sg_info["GroupId"],
+        FromPort=cp.getint("cluster", "db_port"),
+        ToPort=cp.getint("cluster", "db_port")
+    )
+
+    # Internet gateway
+    logger.info("Create internet gateway and attach it to VPC")
     igw_info = ec2_client.create_internet_gateway(
         TagSpecifications=[
             {
                 "ResourceType": "internet-gateway",
-                "Tags":[{"Key": "Name", "Value": config.get("network.vpc", "igw_name")}]
+                "Tags":[{"Key": "Name", "Value": cp.get("network.vpc", "igw_name")}]
             }
         ]
     )["InternetGateway"]
@@ -88,53 +88,57 @@ def create_vpc():
         VpcId=vpc_info["VpcId"]
     )
 
-    logger.info("Create security group and traffic ingress policy")
-    sg_info = ec2_client.create_security_group(
-        GroupName=config.get("network.vpc", "sg_name"),
-        Description="traffic rules over Redshift cluster",
-        VpcId=vpc_info["VpcId"]
-    )
-    ec2_client.authorize_security_group_ingress(
-        CidrIp="0.0.0.0/0",  # better to allow connection by predefined VPN, but skip for simplicity
-        IpProtocol="tcp",
-        GroupId=sg_info["GroupId"],
-        FromPort=config.getint("DEFAULT", "dw_port"),
-        ToPort=config.getint("DEFAULT", "dw_port")
-    )
-
-    logger.info("Create subnet and attach route table")
-    subnet_info = ec2_client.create_subnet(
-        CidrBlock=config.get("network.subnet", "cidr"),
-        VpcId=vpc_info["VpcId"],
-        AvailabilityZone=config.get("network.subnet", "az"),
-        TagSpecifications=[
-            {
-                "ResourceType": "subnet",
-                "Tags": [{"Key": "Name", "Value": config.get("network.subnet", "name")}]
-            }
-        ]
-    )["Subnet"]
+    # Route table
+    logger.info("Create route table and define route from subnet to internet")
     rt_info = ec2_client.create_route_table(
         VpcId=vpc_info["VpcId"],
         TagSpecifications=[
             {
                 "ResourceType": "route-table", 
-                "Tags":[{"Key": "Name", "Value": config.get("network.subnet", "rt_name")}]
+                "Tags":[{"Key": "Name", "Value": cp.get("network.subnet.a", "rt_name")}]
             }
         ]
     )["RouteTable"]
-
-    logger.info("Associate route table to subnet and create route to internet gateway")
-    associtaion = ec2_client.associate_route_table(
-        RouteTableId=rt_info["RouteTableId"], 
-        SubnetId=subnet_info["SubnetId"],
-    )
     ec2_client.create_route(
         DestinationCidrBlock="0.0.0.0/0",
         GatewayId=igw_info["InternetGatewayId"],
         RouteTableId=rt_info["RouteTableId"]
     )
 
+    # Subnets
+    logger.info("Create 2 subnets and attach each to route table")
+    subnet_a_info = ec2_client.create_subnet(
+        CidrBlock=cp.get("network.subnet.a", "cidr"),
+        VpcId=vpc_info["VpcId"],
+        AvailabilityZone=cp.get("network.subnet.a", "az"),
+        TagSpecifications=[
+            {
+                "ResourceType": "subnet",
+                "Tags": [{"Key": "Name", "Value": cp.get("network.subnet.a", "name")}]
+            }
+        ]
+    )["Subnet"]
+    subnet_c_info = ec2_client.create_subnet(
+        CidrBlock=cp.get("network.subnet.c", "cidr"),
+        VpcId=vpc_info["VpcId"],
+        AvailabilityZone=cp.get("network.subnet.c", "az"),
+        TagSpecifications=[
+            {
+                "ResourceType": "subnet",
+                "Tags": [{"Key": "Name", "Value": cp.get("network.subnet.c", "name")}]
+            }
+        ]
+    )["Subnet"]
+    association_a = ec2_client.associate_route_table(
+        RouteTableId=rt_info["RouteTableId"], 
+        SubnetId=subnet_a_info["SubnetId"],
+    )
+    association_c = ec2_client.associate_route_table(
+        RouteTableId=rt_info["RouteTableId"], 
+        SubnetId=subnet_c_info["SubnetId"],
+    )
+
+    # Configuration
     logger.info("Add resource IDs into configuration file")
     network_resource_info = {
         "network.vpc": {
@@ -142,20 +146,80 @@ def create_vpc():
             "igw_id": igw_info["InternetGatewayId"],
             "sg_id": sg_info["GroupId"],
         },
-        "network.subnet": {
-            "id": subnet_info["SubnetId"],
+        "network.subnet.a": {
+            "id": subnet_a_info["SubnetId"],
             "rt_id": rt_info["RouteTableId"],
-            "rt_asc_id": associtaion["AssociationId"]
+            "rt_asc_id": association_a["AssociationId"]
+        },
+        "network.subnet.c": {
+            "id": subnet_c_info["SubnetId"],
+            "rt_id": rt_info["RouteTableId"],
+            "rt_asc_id": association_c["AssociationId"]
         }
     }
-    config.read_dict(network_resource_info)
+    cp.read_dict(network_resource_info)
     with open(CONFIG_FILE_PATH, "w") as file:
-        config.write(file)
+        cp.write(file)
 
 
 @app.command("create-cluster")
-def create_redshift_cluster():
-    pass
+def create_redshift_cluster(
+    db_password: str = typer.Argument(...)
+):
+    cp.read(CONFIG_FILE_PATH)
+    assert cp.has_option("network.vpc", "id"), \
+        "This process requires predefined subnets. Execute create-vpc command first."
+
+    if cp.has_option("cluster", "id"):
+        logger.info("Terminate process since Redshift cluster is already created")
+        exit()
+
+    session = boto3.Session(
+        profile_name=cp.get("DEFAULT", "admin_profile"), 
+        region_name=cp.get("DEFAULT", "region")
+    )
+    redshift_client = session.client("redshift")
+
+    logger.info("Create subnet group")
+    redshift_client.create_cluster_subnet_group(
+        ClusterSubnetGroupName=cp.get("cluster.subnet.group", "name"),
+        Description=cp.get("cluster.subnet.group", "desc"),
+        SubnetIds=[
+            cp.get("network.subnet.a", "id"),
+            cp.get("network.subnet.c", "id")
+        ]
+    )
+    
+    logger.info("Create redshift cluster")
+    response = redshift_client.create_cluster(
+        ClusterIdentifier=cp.get("cluster", "identifier"),
+        Port=cp.getint("cluster", "db_port"),
+        DBName=cp.get("cluster", "db_name"),
+        MasterUsername=cp.get("DEFAULT", "admin_profile"),
+        MasterUserPassword=db_password,
+        ClusterType="multi-node",
+        NodeType=cp.get("cluster", "node_type"),
+        NumberOfNodes=cp.getint("cluster", "node_count"),
+        ClusterSubnetGroupName=cp.get("cluster.subnet.group", "name"),
+        AvailabilityZone=cp.get("network.subnet.a", "az"),
+        PubliclyAccessible=True,
+        EnhancedVpcRouting=False,
+        VpcSecurityGroupIds=[cp.get("network.vpc", "sg_id")],
+    )
+    
+    logger.info("Waiting for cluster to be available")
+    cluster_status = response["Cluster"]["ClusterStatus"]
+    while cluster_status != "available":
+        time.sleep(5)
+        cluster_info = redshift_client.describe_clusters(
+            ClusterIdentifier=cp.get("cluster", "identifier")
+        )["Clusters"][0]
+        cluster_status = cluster_info["ClusterStatus"]
+    
+    logger.info("Save DB host information in configuration file")
+    cp["cluster"]["db_host"] = cluster_info["Endpoint"]["Address"]
+    with open(CONFIG_FILE_PATH, "w") as file:
+        cp.write(file)
 
 
 @app.command("create-iam")
@@ -165,50 +229,99 @@ def create_iam():
 
 @app.command("delete-vpc")
 def delete_vpc():
-    config.read(CONFIG_FILE_PATH)
-    if not config.has_option("network.vpc", "id"):
+    cp.read(CONFIG_FILE_PATH)
+    if not cp.has_option("network.vpc", "id"):
         logger.info("VPC to be deleted is not defined in configuration file")
         exit()
     session = boto3.Session(
-        profile_name=config.get("DEFAULT", "admin_profile"), 
-        region_name=config.get("DEFAULT", "region")
+        profile_name=cp.get("DEFAULT", "admin_profile"), 
+        region_name=cp.get("DEFAULT", "region")
     )
     ec2_client = session.client("ec2")
 
-    logger.info("Disassociate route table from subnet")
+    # Subnets
+    logger.info("Disassociate route table from subnets and delete 2 subnets")
     ec2_client.disassociate_route_table(
-        AssociationId=config.get("network.subnet", "rt_asc_id")
+        AssociationId=cp.get("network.subnet.a", "rt_asc_id")
     )
+    ec2_client.disassociate_route_table(
+        AssociationId=cp.get("network.subnet.c", "rt_asc_id")
+    )
+    ec2_client.delete_subnet(SubnetId=cp.get("network.subnet.a", "id"))
+    ec2_client.delete_subnet(SubnetId=cp.get("network.subnet.c", "id"))
 
-    logger.info("Delete route table and corresponding subnet")
+    # Route table
+    logger.info("Delete route table")
     ec2_client.delete_route_table(
-        RouteTableId=config.get("network.subnet", "rt_id")
+        RouteTableId=cp.get("network.subnet.a", "rt_id")
     )
-    ec2_client.delete_subnet(SubnetId=config.get("network.subnet", "id"))
 
-    logger.info("Delete security group defined for current VPC")
-    ec2_client.delete_security_group(GroupId=config.get("network.vpc", "sg_id"))
-
-    logger.info("Detach internet gateway and delete VPC")
+    # Internet gateway
+    logger.info("Detach internet gateway from VPC and delete it")
     ec2_client.detach_internet_gateway(
-        InternetGatewayId=config.get("network.vpc", "igw_id"), 
-        VpcId=config.get("network.vpc", "id")
+        InternetGatewayId=cp.get("network.vpc", "igw_id"), 
+        VpcId=cp.get("network.vpc", "id")
     )
     ec2_client.delete_internet_gateway(
-        InternetGatewayId=config.get("network.vpc", "igw_id")
-    )    
-    ec2_client.delete_vpc(VpcId=config.get("network.vpc", "id"))
+        InternetGatewayId=cp.get("network.vpc", "igw_id")
+    )
 
+    # Security group
+    logger.info("Delete security group defined for current VPC")
+    ec2_client.delete_security_group(GroupId=cp.get("network.vpc", "sg_id"))
+
+    # VPC
+    logger.info("Delete VPC")
+    ec2_client.delete_vpc(VpcId=cp.get("network.vpc", "id"))
+
+    # Configuration
     logger.info("Delete resource IDs from configuration file")
-    config.remove_option("network.vpc", "id")
-    config.remove_option("network.vpc", "igw_id")
-    config.remove_option("network.vpc", "sg_id")
-    config.remove_option("network.subnet", "id")
-    config.remove_option("network.subnet", "rt_id")
-    config.remove_option("network.subnet", "rt_asc_id")
+    cp.remove_section("network.vpc")
+    cp.remove_section("network.subnet.a")
+    cp.remove_section("network.subnet.c")
     with open(CONFIG_FILE_PATH, "w") as file:
-        config.write(file)
+        cp.write(file)
 
+
+@app.command("delete-cluster")
+def delete_redshift_cluster():
+    cp.read(CONFIG_FILE_PATH)
+    if not cp.has_option("cluster", "db_host"):
+        logger.info("Cluster to be deleted is not defined in configuration file")
+        exit()
+    session = boto3.Session(
+        profile_name=cp.get("DEFAULT", "admin_profile"), 
+        region_name=cp.get("DEFAULT", "region")
+    )
+    redshift_client = session.client("redshift")
+
+    # Cluster
+    logger.info("Delete Redshift cluster")
+    redshift_client.delete_cluster(
+        ClusterIdentifier=cp.get("cluster", "identifier"),
+        SkipFinalClusterSnapshot=True
+    )
+    logger.info("Waiting for cluster to be deleted")
+    cluster_status = "deleting"
+    while cluster_status == "deleting":
+        time.sleep(5)
+        try:
+            redshift_client.describe_clusters(
+                ClusterIdentifier=cp.get("cluster", "identifier")
+            )
+        except redshift_client.exceptions.ClusterNotFoundFault:
+            cluster_status = "deleted" 
+    
+    # Cluster subnet group
+    logger.info("Delete corresponding subnet group")
+    redshift_client.delete_cluster_subnet_group(
+        ClusterSubnetGroupName=cp.get("cluster.subnet.group", "name")
+    )
+
+    # Configuration
+    logger.info("Delete resource IDs from configuration file")
+    cp.remove_section("cluster")
+    cp.remove_section("cluster.subnet.group")
 
 if __name__ == "__main__":
     app()
